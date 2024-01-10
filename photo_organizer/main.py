4b7005e2c72119691790b78970457131f58f19e1
peterkost <46546031+peterkost@users.noreply.github.com>
2024-01-10 16:41:13 -0500

    Added pickling

--- photo_organizer/main.py
+++ photo_organizer/main.py
@@ -2,21 +2,46 @@ from file_reader import getFilesInArgDir
 from file_sorter import sortFiles
 from photo_processor import addMetaDataToPhotos
 from pillow_heif import register_heif_opener
+import pickle
 #from video_processor import processVideos
+
+
+usePickle = True
+
 
 def main():
     register_heif_opener()
 
-    filePaths = getFilesInArgDir()
-    groupedFiles = sortFiles(filePaths)
-    noDate = 0
+    
+    if usePickle:
+        groupedFiles = unpickle()
+        
+    else:
+        filePaths = getFilesInArgDir()
+        groupedFiles = sortFiles(filePaths)
+        funniestShitIveEverSeen(groupedFiles)
+
+    noDate = date = 0
     for photo in groupedFiles:
         if not photo.dateTime:
             noDate += 1
             print(photo.paths[0].path)
+        else:
+            date += 1
     #processedPhotos = addMetaDataToPhotos(groupedFiles.photos)
+    print("date", date, "nodate", noDate)
 
 
+def funniestShitIveEverSeen(obj):
+    file = open('funnestShitIveEverSeen', 'ab')
+    pickle.dump(obj, file)
+    file.close()
+
+def unpickle():
+    file = open("funnestShitIveEverSeen", 'rb')
+    p = pickle.load(file)
+    file.close()
+    return p
 
 if __name__ == "__main__":
     main()
